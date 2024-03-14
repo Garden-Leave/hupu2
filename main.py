6446b4dbdb885777a8690b9071df6edd67128fa5
Administrator <hslb@163.com>
2024-03-14 01:49:37 +0000

    update master for g1 update

--- main.py
+++ main.py
@@ -7,26 +7,29 @@ from flask_cors import CORS, cross_origin   #解决跨域问题会在response he
 from helpers.fetch_parse import fetch_parse
 from prometheus_client import start_http_server
 from helpers.metrics import c1
-import requests
-from helpers.log_helper import logger
-
-
+import requests, os
+from helpers.log_helper import logger1
+import traceback
 
 app = Flask('jack')
+
 
 @app.route('/hupu', methods=['GET'], endpoint='hp')
 @cross_origin(origins='*')    # 允许跨域
 def get_hp():
-    num = int(request.args.get('num'))
-    app.logger.info(num)
-    list2 = fetch_parse()
-    # app.logger.info(list2)
-    result = list2[0:num*2]
-    print(request.environ.items())
-    log_req_record=[request.environ['REQUEST_URI'], request.environ["REQUEST_METHOD"], request.environ['SERVER_PROTOCOL'],request.environ['REMOTE_ADDR'],request.environ['HTTP_USER_AGENT']]
-    # logger.warning(log_req_record)
-    response = make_response(jsonify(result), 200, {"header1": "jordan"})
-    logger.info('request info: {} status_code: {}'.format(log_req_record, response.status_code))
+    num = int(request.args.get('num'))   # 必须转换为整数
+    if num <= 0:
+        response = make_response('400：Bad Request, number should be > 0 ', 400, {"header": "test header"})
+        logger1.info('400：Bad Request, number should be > 0 status_code: {}'.format(response.status_code))
+    else:
+        fetch_parse(num)
+        # result = list2[0:num*2]
+        #print(request.environ.items())
+        log_req_record=[request.environ['REQUEST_URI'], request.environ["REQUEST_METHOD"], request.environ['SERVER_PROTOCOL'],request.environ['REMOTE_ADDR'],request.environ['HTTP_USER_AGENT']]
+        # logger.warning(log_req_record)
+        response = make_response(render_template('result.html'), 200, {"header": "test header"})
+        #response = make_response(jsonify(result), 200, {"header1": "jordan"})
+        logger1.info('request info: {} status_code: {}'.format(log_req_record, response.status_code))
 
     return response
 
@@ -36,17 +39,20 @@ def get_hp():
 def hello():
     log_req_record = [request.environ['REQUEST_URI'], request.environ["REQUEST_METHOD"], request.environ['SERVER_PROTOCOL'],request.environ['REMOTE_ADDR'],request.environ['HTTP_USER_AGENT']]
     response = make_response(render_template('hello.html'), 200, {"header": "test header"})
-    logger.info('request info: {} status_code: {}'.format(log_req_record, response.status_code))
+    logger1.info('request info: {} status_code: {}'.format(log_req_record, response.status_code))
     return response
+
 
 @app.route('/nginx', methods=['GET'], endpoint='nginx')
 @cross_origin(origins='*')
 def nginx():
     log_req_record=[request.environ['REQUEST_URI'], request.environ["REQUEST_METHOD"], request.environ['SERVER_PROTOCOL'],request.environ['REMOTE_ADDR'],request.environ['HTTP_USER_AGENT']]
-    response = make_response(requests.get('http://ng:8000').text, 200, {"header": "test header"})
-    logger.info('request info: {} status_code: {}'.format(log_req_record, response.status_code))
-    return response
-
+    try:
+       response = make_response(requests.get('http://ng.default.svc.cluster.local:8000').text, 200, {"header": "test header"})
+       logger1.info('request info: {} status_code: {}'.format(log_req_record, response.status_code))
+       return response
+    except Exception as e:
+        logger1.info(e)
 
 
 
@@ -54,18 +60,16 @@ def nginx():
 @cross_origin(origins='*')
 def hello():
     log_req_record=[request.environ['RAW_URI'],request.environ['REMOTE_ADDR'],request.environ['HTTP_USER_AGENT'],request.environ['RAW_URI']]
-    logger.info(log_req_record)
+    logger1.info(log_req_record)
     return redirect('hello')
 
 
 @app.after_request
 def metric_write(response):
     # print('after request')
-    c1.labels(pod='pod1', node='node1', code=response.status_code).inc(1)
+    c1.labels(hostname=os.getenv('HOSTNAME'), node='node1', code=response.status_code).inc(1)
     # logger.info('prometheus metric request_total written')
     return response
-
-
 
 
 # @app.after_request    #  after request函数接收response后记得再return回去 不然就空了
@@ -73,20 +77,13 @@ def metric_write(response):
 #     # print('after request')
 #     app.logger.warning(f'response data logged in after request func:{response.status}')
 #     return response
-# # @app.before_request
-
 
 if __name__ == '__main__':
     start_http_server(9090)
     try:
-            app.logger.info('starting app using logger ')
-            logger.info('now starting app')
-            app.run('0.0.0.0')
+        app.run()
+        logger1.debug('flask main app started')
     except Exception as e:
-            import traceback
-            app.logger.error(f'flask main app start failed:{e}')
+        logger1.error(f'flask main app start failed:{e}')
 
-    finally:
-            app.logger.info('app stopped')
 
-    # run.send_mail()
